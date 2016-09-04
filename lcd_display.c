a57e268194cd851ebd593af00e2def30d6be5f01
Brendan Duke <dukebw@mcmaster.ca>
2016-09-04 16:07:02 -0400

    LCD Display UVa 706 Accepted

--- lcd_display.c
+++ lcd_display.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
+#include <stdbool.h>
 
 #define ROWS (2 + 3)
 #define COLUMNS (1 + 2)
@@ -15,8 +17,22 @@ typedef struct lcd_digit
     char Row4;
 } lcd_digit;
 
-static lcd_digit GlobalDigits[] =
+static lcd_digit
+GlobalDigits[] =
 {
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            '|', '|',
+        },
+        .Row2 = ' ',
+        .Row3 =
+        {
+            '|', '|',
+        },
+        .Row4 = '-',
+    },
     {
         .Row0 = ' ',
         .Row1 =
@@ -29,6 +45,110 @@ static lcd_digit GlobalDigits[] =
             ' ', '|',
         },
         .Row4 = ' ',
+    },
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            ' ', '|',
+        },
+        .Row2 = '-',
+        .Row3 =
+        {
+            '|', ' ',
+        },
+        .Row4 = '-',
+    },
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            ' ', '|',
+        },
+        .Row2 = '-',
+        .Row3 =
+        {
+            ' ', '|',
+        },
+        .Row4 = '-',
+    },
+    {
+        .Row0 = ' ',
+        .Row1 =
+        {
+            '|', '|',
+        },
+        .Row2 = '-',
+        .Row3 =
+        {
+            ' ', '|',
+        },
+        .Row4 = ' ',
+    },
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            '|', ' ',
+        },
+        .Row2 = '-',
+        .Row3 =
+        {
+            ' ', '|',
+        },
+        .Row4 = '-',
+    },
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            '|', ' ',
+        },
+        .Row2 = '-',
+        .Row3 =
+        {
+            '|', '|',
+        },
+        .Row4 = '-',
+    },
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            ' ', '|',
+        },
+        .Row2 = ' ',
+        .Row3 =
+        {
+            ' ', '|',
+        },
+        .Row4 = ' ',
+    },
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            '|', '|',
+        },
+        .Row2 = '-',
+        .Row3 =
+        {
+            '|', '|',
+        },
+        .Row4 = '-',
+    },
+    {
+        .Row0 = '-',
+        .Row1 =
+        {
+            '|', '|',
+        },
+        .Row2 = '-',
+        .Row3 =
+        {
+            ' ', '|',
+        },
+        .Row4 = '-',
     },
 };
 
@@ -44,24 +164,72 @@ PrintMiddleOfRow(char MiddleOfRowChar, uint32_t Dashes)
 }
 
 static void
-PrintEvenRow(char MiddleOfRowChar, uint32_t Dashes)
+PrintDigitEvenRow(char MiddleOfRowChar, uint32_t Dashes)
 {
     printf(" ");
     PrintMiddleOfRow(MiddleOfRowChar, Dashes);
-    printf(" \n");
+    printf(" ");
 }
 
 static void
-PrintOddRow(char LeftSideChar, char RightSideChar, uint32_t Dashes)
+PrintDigitOddRow(char LeftSideChar, char RightSideChar, uint32_t Dashes)
 {
-    for (uint32_t DashesIndex = 0;
-         DashesIndex < Dashes;
-         ++DashesIndex)
+    printf("%c", LeftSideChar);
+    PrintMiddleOfRow(' ', Dashes);
+    printf("%c", RightSideChar);
+}
+
+static void
+PrintRow(lcd_digit *Digits, uint32_t *DigitsToDisplay, uint32_t TotalDigits, uint32_t Dashes, uint32_t RowNumber)
+{
+    assert(TotalDigits > 0);
+
+    for (int32_t DigitIndex = (TotalDigits - 1);
+         DigitIndex >= 0;
+         --DigitIndex)
     {
-        printf("%c", LeftSideChar);
-        PrintMiddleOfRow(' ', Dashes);
-        printf("%c\n", RightSideChar);
+        uint32_t NextDigit = DigitsToDisplay[DigitIndex];
+        switch (RowNumber)
+        {
+            case 0:
+            {
+                PrintDigitEvenRow(Digits[NextDigit].Row0, Dashes);
+                break;
+            }
+            case 1:
+            {
+                PrintDigitOddRow(Digits[NextDigit].Row1[0], Digits[NextDigit].Row1[1], Dashes);
+                break;
+            }
+            case 2:
+            {
+                PrintDigitEvenRow(Digits[NextDigit].Row2, Dashes);
+                break;
+            }
+            case 3:
+            {
+                PrintDigitOddRow(Digits[NextDigit].Row3[0], Digits[NextDigit].Row3[1], Dashes);
+                break;
+            }
+            case 4:
+            {
+                PrintDigitEvenRow(Digits[NextDigit].Row4, Dashes);
+                break;
+            }
+            default:
+            {
+                assert(false);
+                break;
+            }
+        }
+
+        if (0 < DigitIndex)
+        {
+            printf(" ");
+        }
     }
+
+    printf("\n");
 }
 
 int main(void)
@@ -73,10 +241,50 @@ int main(void)
            (Dashes <= 10) &&
            (NumberToDisplay <= 99999999))
     {
-        PrintEvenRow(GlobalDigits[0].Row0, Dashes);
-        PrintOddRow(GlobalDigits[0].Row1[0], GlobalDigits[0].Row1[1], Dashes);
-        PrintEvenRow(GlobalDigits[0].Row2, Dashes);
-        PrintOddRow(GlobalDigits[0].Row3[0], GlobalDigits[0].Row3[1], Dashes);
-        PrintEvenRow(GlobalDigits[0].Row4, Dashes);
+        uint32_t DigitsToDisplay[8];
+        uint32_t TotalDigits = 0;
+        do
+        {
+            uint32_t NextDigit = (NumberToDisplay % 10);
+            DigitsToDisplay[TotalDigits++] = NextDigit;
+            NumberToDisplay /= 10;
+        } while (0 < NumberToDisplay);
+
+        for (uint32_t LineIndex = 0;
+             LineIndex < (2*Dashes + 3);
+             ++LineIndex)
+        {
+            // 0                            -> 0
+            // 1..Dashes                    -> 1
+            // Dashes + 1                   -> 2
+            // (Dashes + 2)..(2*Dashes + 1) -> 3
+            // (2*Dashes + 2)               -> 4
+            uint32_t RowIndex;
+            if (0 == LineIndex)
+            {
+                RowIndex = 0;
+            }
+            else if (Dashes >= LineIndex)
+            {
+                RowIndex = 1;
+            }
+            else if ((Dashes + 1) == LineIndex)
+            {
+                RowIndex = 2;
+            }
+            else if ((2*Dashes + 1) >= LineIndex)
+            {
+                RowIndex = 3;
+            }
+            else
+            {
+                assert((2*Dashes + 2) == LineIndex);
+                RowIndex = 4;
+            }
+
+            PrintRow(GlobalDigits, DigitsToDisplay, TotalDigits, Dashes, RowIndex);
+        }
+
+        printf("\n");
     }
 }
